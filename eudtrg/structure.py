88d85d658413bcbd3c3293fa07ca4e763000273b
phu54321 <phu54321@naver.com>
2014-11-24 10:39:40 +0900

    Initial structure design design
    cc

---
+++ eudtrg/structure.py
@@ -0,0 +1,42 @@
+'''
+#Utils
+    binio, ubconv, FlattenList,
+
+#MapData
+    MapStringParsers
+    ConstParsers
+
+#EUDObject
+    Expr
+    Forward
+    EUDObject
+    Db
+
+#Trigger
+    Trigger
+    Condition
+    Action
+    Disabled
+
+    TriggerScope
+    NextTrigger
+
+#CtrlStru
+    EUDFunc
+    VTable
+    Ctrlstru
+    BlockStru
+
+#EUDFunc
+
+#MpqRW
+    mpqapi
+    chktok
+
+#MapRead
+
+
+#MapWrite
+    injgen
+    DoEvents
+'''
