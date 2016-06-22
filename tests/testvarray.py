df57c8979b17eb4952704aa2a5b80d90c434db56
phu54321 <phu54321@naver.com>
2016-06-22 16:10:32 +0900

    More testing

--- tests/testvarray.py
+++ tests/testvarray.py
@@ -10,14 +10,19 @@ LoadMap('outputmap/basemap/basemap.scx')
 
 @EUDFunc
 def main():
-    a = EUDVArray(10)
-    for i in range(10):
+    a = EUDVArray([5, 5, 5, 5, 5, 5, 5, 5, 5, 5])
+    for i in range(8):
         a[i] = 2 ** i
+
+    for i in EUDLoopRange(3, 6):
+        a[i] = i * i * i
 
     b = EUDVariable(a)
     c = EUDVArray(b)
 
-    for i in range(10):
+    for i in EUDLoopRange(0, 8):
         f_simpleprint(c[i])
+
+    f_simpleprint(a[9])
 
 SaveMap('outputmap\\testvarray.scx', main)
