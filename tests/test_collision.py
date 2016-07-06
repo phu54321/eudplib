c1437bcb8aa3fcc09c56c1add0ec930c4aa13f90
phu54321 <phu54321@naver.com>
2016-07-06 19:19:09 +0900

    test_collision : complex struct-using test case

---
+++ tests/test_collision.py
@@ -0,0 +1,183 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath('..\\'))
+
+from eudplib import *
+
+
+class Vector2D(EUDStruct):
+    _fields_ = ['x', 'y']
+
+    def __init__(self, x=None, y=None):
+        if y is None:
+            super().__init__(x)
+        else:
+            super().__init__([x, y])
+
+    def lengthsq(self):
+        return self.x * self.x + self.y * self.y
+
+    def length(self):
+        return f_sqrt(self.lengthsq())
+
+    def dot(self, rhs):
+        return self.x * rhs.x + self.y * rhs.y
+
+    # Operator support
+
+    def add(self, rhs):
+        t = self.clone()
+        t += rhs
+        return t
+
+    def sub(self, rhs):
+        t = self.clone()
+        t -= rhs
+        return t
+
+    def mul(self, rhs):
+        t = self.clone()
+        t *= rhs
+        return t
+
+    def div(self, rhs):
+        t = self.clone()
+        t //= rhs
+        return t
+
+    def __iadd__(self, rhs):
+        self.x += rhs.x
+        self.y += rhs.y
+        return self
+
+    def __isub__(self, rhs):
+        print(self, rhs)
+        self.x -= rhs.x
+        self.y -= rhs.y
+        return self
+
+    def __imul__(self, rhs):
+        self.x *= rhs
+        self.y *= rhs
+        return self
+
+    def __ifloordiv__(self, rhs):
+        if EUDIf()(self.x >= 0x80000000):
+            self.x = -(-self.x // rhs)
+        if EUDElse()():
+            self.x //= rhs
+        EUDEndIf()
+
+        if EUDIf()(self.y >= 0x80000000):
+            self.y = -(-self.y // rhs)
+        if EUDElse()():
+            self.y //= rhs
+        EUDEndIf()
+
+        return self
+
+
+radius = 100
+
+
+class CircleObj(EUDStruct):
+    _fields_ = [
+        ('pos', Vector2D),
+        ('velocity', Vector2D),
+    ]
+
+    def __init__(self, copyobj=None, *, pos=None, velocity=None):
+        if copyobj is not None:
+            super().__init__(copyobj)
+
+        else:
+            if pos is None:
+                pos = Vector2D()
+            if velocity is None:
+                velocity = Vector2D()
+            super().__init__([pos, velocity])
+
+    @EUDFuncMethod
+    def collides(self, rhs):
+        self = CircleObj(self)
+        rhs = CircleObj(rhs)
+
+        print(rhs.pos)
+        print(self.pos)
+        print(rhs.pos - self.pos)
+
+        distsq = rhs.pos.sub(self.pos).lengthsq()
+        rsum = radius + radius
+        rsumsq = rsum * rsum
+        if EUDIf()(distsq <= rsumsq):
+            EUDReturn(1)
+        if EUDElse()():
+            EUDReturn(0)
+        EUDEndIf()
+
+    def step(self):
+        self.pos += self.velocity
+
+    @EUDFuncMethod
+    def applyCollision(self, rhs):
+        self = CircleObj(self)
+        rhs = CircleObj(rhs)
+        if EUDIf()(self.collides(rhs)):
+            posdiff = rhs.pos.sub(self.pos)
+            velavg = rhs.velocity.add(self.velocity).div(2)
+            veldiff = rhs.velocity.sub(self.velocity)
+
+            # Check if posdiff is in align with veldiff with dot
+            aligning = posdiff.dot(veldiff)
+            if EUDIf()(aligning <= 0x80000000):
+                # Veldiff and posdiff are in opposite direction.
+                # Meaning that they are doing collision
+
+                # We create normal vector perpendicular with posdiff.
+                posdiff_perpend = Vector2D()
+                posdiff_perpend.x = -posdiff.y
+                posdiff_perpend.y = posdiff.x
+                posdiff_length = posdiff.length()
+
+                # Divide velavg to perpendicular segment and parallel segment
+                para_length = aligning
+                velavg.dot(veldiff)
+                
+                perp_length = posdiff_perpend.dot(veldiff)
+
+                # Invert para_length : collision
+                para_length = -para_length
+
+                # calculate veldiff back
+                new_veldiff = (
+                    posdiff.mul(para_length).add(
+                        posdiff_perpend.mul(perp_length))
+                ).div(posdiff_length * posdiff_length)
+                new_veldiff.deepcopy(veldiff)
+
+                veldiff_d2 = veldiff.div(2)
+
+                # Calculate self & rhs's velocity
+                velavg.sub(veldiff_d2).deepcopy(self.velocity)
+                velavg.add(veldiff_d2).deepcopy(rhs.velocity)
+            EUDEndIf()
+        EUDEndIf()
+
+
+@EUDFunc
+def main():
+    a = CircleObj(pos=Vector2D(0, 0), velocity=Vector2D(10, 0))
+    b = CircleObj(pos=Vector2D(1000, 0), velocity=Vector2D(-10, 0))
+
+    if EUDInfLoop()():
+        a.applyCollision(b)
+        a.step()
+        b.step()
+        f_simpleprint(a.pos.x, a.pos.y, b.pos.x, b.pos.y)
+        DoActions(SetMemory(0x6509A0, SetTo, 0))
+        EUDDoEvents()
+    EUDEndInfLoop()
+
+LoadMap("outputmap/basemap/basemap.scx")
+SaveMap("outputmap/collision.scx", main)
