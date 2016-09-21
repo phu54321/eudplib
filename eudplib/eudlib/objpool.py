1c8ea29645672caea91259bdd68e6e5ad19b8040
phu54321 <phu54321@naver.com>
2016-09-22 03:21:32 +0900

    Pool completed

--- eudplib/eudlib/objpool.py
+++ eudplib/eudlib/objpool.py
@@ -1,97 +1,45 @@
 from .. import (
     core as c,
-    utils as ut,
     ctrlstru as cs,
 )
 
-
-class PoolEntry(c.EUDStruct):
-    pass
-
-PoolEntry._fields_ = [
-    ('prev', PoolEntry),
-    ('next', PoolEntry),
-    ('value', PoolEntry),
-]
+from .eudarray import EUDArray
 
 
 class ObjPool(c.EUDStruct):
     _fields_ = [
-        ('_emptyhead', PoolEntry),
-        ('_usedhead', PoolEntry),
+        ('data', EUDArray),
+        'remaining',
+        'size'
     ]
 
     def __init__(self, size, basetype=None):
-        if basetype is None:
-            super().__init__(size)
-            return
-
-        # Create entries
-        emptysize = size + 1
-        entrylist = [c.Forward() for _ in range(emptysize)]
-        entryprev = [entrylist[i - 1] for i in range(emptysize)]
-        entrynext = [entrylist[(i + 1) % emptysize] for i in range(emptysize)]
-        entryvalue = [0] + [basetype() for _ in range(emptysize)]
-
-        for i in range(emptysize):
-            entrylist[i] << PoolEntry([
-                entryprev[i],
-                entrynext[i],
-                entryvalue[i]
-            ])
-
-        emptyhead = entrylist[0]
-
-        usedhead = c.Forward()
-        usedhead << PoolEntry([usedhead, usedhead, 0])
-
+        objects = [basetype() for _ in range(size)]
         super().__init__([
-            emptyhead,
-            usedhead
+            EUDArray(objects),
+            size,
+            size
         ])
+        self._basetype = basetype
 
-    def useditems(self):
-        """ Iterator through used items """
-        usedhead = self._usedhead
-        ptr = usedhead.next
-        if cs.EUDWhileNot()(ptr == usedhead):
-            yield ptr
-            ptr << ptr.next
-        cs.EUDEndWhile()
+    def empty(self):
+        return self.remaining == 0
 
+    @c.EUDMethod
     def alloc(self):
         """ Allocate one object from pool """
-
-        # Pop one from empty list
-        emptyhead = self._emptyhead
-        ptr = emptyhead.next.asVariable()
-        if cs.EUDIfNot()(ptr == emptyhead):
-            ptr.next.prev = ptr.prev.next
-            ptr.prev.next = ptr.next.prev
-
-            # Put to used list
-            usedhead = self._usedhead
-            ptr.prev = usedhead.prev
-            ptr.next = usedhead.next
-            usedhead.prev.next = ptr
-            usedhead.prev = ptr
-        if cs.EUDElse()():
-            ptr << 0
+        if cs.EUDIf()(self.empty()):
+            c.EUDReturn(0)
         cs.EUDEndIf()
 
-        return ptr
+        self.remaining -= 1
+        data = self.data[self.remaining]
+        return data
 
-    def free(self, entry):
-        # Remove from used list
-        entry.next.prev = entry.prev
-        entry.prev.next = entry.next
-
-        # Add to empty list
-        emptyhead = self._emptyhead
-        entry.next = emptyhead.next
-        entry.prev = emptyhead
-        emptyhead.next.prev = entry
-        emptyhead.next = entry
+    @c.EUDMethod
+    def free(self, data):
+        self.data[self.remaining] = data
+        self.remaining += 1
 
     def clone(self):
         raise RuntimeError('Pool is not clonable')
