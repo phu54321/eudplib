709f60847b7cf7a912cf3c4c6b8428beea7a5f77
phu54321 <phu54321@naver.com>
2016-10-09 14:45:01 +0900

    Revert "Removed cython & added cffi version of stackobjs"
    This reverts commit 34bfa579c836cc1bec46dd2d21b369856f91b30e.

---
+++ cython_src/testverifyf.py
@@ -0,0 +1,21 @@
+import pyximport
+pyximport.install()
+
+import verifyf
+import os
+
+testvector = bytearray(os.urandom(10))
+
+
+import cProfile
+import pstats
+
+cProfile.runctx(
+    'verifyf.verifyf(testvector, len(testvector))',
+    globals(),
+    locals(),
+    "Profile.prof"
+)
+
+s = pstats.Stats("Profile.prof")
+s.strip_dirs().sort_stats('time').print_stats()
